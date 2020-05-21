b0d1e62f1bac0a5e9d454e45e01af9cfaf401e52
SandraGpe <14980671@itroque.edu.mx>
2020-05-21 16:37:49 -0500

    update 4

--- app/src/main/java/com/example/bakerysraul/MainActivity.kt
+++ app/src/main/java/com/example/bakerysraul/MainActivity.kt
@@ -3,16 +3,48 @@ package com.example.bakerysraul
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_user_perfil.*
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
-
+    val IP = "http://192.168.1.77" // Dirección IP del servidor web que almacena los servicios web
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun empleados(v:View){
+        val wsURL = IP + "/WSBakery/bdLogin.php"
+        val admin = adminbd(this)
+        admin.Ejecuta("DELETE FROM usuario")
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.POST,wsURL,null,
+            Response.Listener { response ->
+                val succ = response["success"]
+                val msg = response["message"]
+                val sensadoJson = response.getJSONArray("empleados")
+                for (i in 0 until sensadoJson.length()){
+                    // Los nombres del getString son como los arroja el servicio web
+                    val corr = sensadoJson.getJSONObject(i).getString("correoUsr")
+                    val contra = sensadoJson.getJSONObject(i).getString("contrasena")
+                    val sentencia = "INSERT INTO usuario(corrUsr,nomusr,contrasena) values(${corr}, '${null}','${contra})"
+                    val res = admin.Ejecuta(sentencia)
+                }
+            },
+            Response.ErrorListener { error ->
+                Toast.makeText(this, "Error Empleados: " + error.message.toString() , Toast.LENGTH_LONG).show();
+                Log.d("GonzalezSandra",error.message.toString() )
+            }
+        )
+        VolleySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+
     fun btnLogin(view: View){
         if (txtCorr.text.isEmpty() || txtContra.text.isEmpty()){
             txtCorr.setError("Error, faltan datos de entrada")
@@ -41,4 +73,6 @@ class MainActivity : AppCompatActivity() {
         val acti : Intent = Intent(this,MainRegistro::class.java)
         startActivity(acti)
     }
+
+   
 }
