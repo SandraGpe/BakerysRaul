adff6dd03ba92ccfb8725fba999be6f2315c70da
SandraGpe <14980671@itroque.edu.mx>
2020-05-28 09:38:03 -0500

    update 13

--- app/src/main/java/com/example/bakerysraul/Pedidos.kt
+++ app/src/main/java/com/example/bakerysraul/Pedidos.kt
@@ -2,11 +2,30 @@ package com.example.bakerysraul
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_pedidos.*
 
 class Pedidos : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pedidos)
+
+        val mes = arrayListOf("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre")
+        val arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,mes)
+
+        spinner.adapter = arrayAdapter
+        spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                fechaEnt.text="elige una opcion"
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                fechaEnt.text = mes[position]
+            }
+        }
     }
 }
