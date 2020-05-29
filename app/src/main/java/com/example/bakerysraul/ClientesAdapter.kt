f128cfb7e2eb4c590cbc8d8e8f696c777c8c3a1f
SandraGpe <14980671@itroque.edu.mx>
2020-05-29 02:32:39 -0500

    update 22

--- app/src/main/java/com/example/bakerysraul/ClientesAdapter.kt
+++ app/src/main/java/com/example/bakerysraul/ClientesAdapter.kt
@@ -2,15 +2,40 @@ package com.example.bakerysraul
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.celda_prototipo_clientes.view.*
 
 class ClientesAdapter (private var mListClientes:List<Clientes>,
 private val mContext: Context, private val clickListener: (Clientes)-> Unit)
     : RecyclerView.Adapter<ClientesAdapter.ClientesViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClientesAdapter.ClientesViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.,parent,false)
-        return recyclerAdapter.recyclerViewHolder(itemView)
+        val  LayoutInflater= LayoutInflater.from(mContext).inflate(R.layout.celda_prototipo_clientes,parent,false)
+        return ClientesAdapter.ClientesViewHolder (LayoutInflater)
+    }
+
+    override fun onBindViewHolder(holder: ClientesAdapter.ClientesViewHolder, position: Int) {
+        holder.bind(mListClientes[position],mContext,clickListener)
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int=mListClientes.size
+    fun setTask(clientes: List<Clientes>){
+        mListClientes = clientes
+        notifyDataSetChanged()
+    }
+
+    fun getTask(): List<Clientes> = mListClientes
+    class ClientesViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        fun bind(estud:Clientes,context: Context,clickListener: (Clientes) -> Unit) {
+            itemView.Nombre.text = estud.nom.toString()
+            itemView.Apellido.text = estud.ape.toString()
+
+            itemView.setOnClickListener{clickListener(estud)}
+            }
     }
 }
--- app/src/main/java/com/example/bakerysraul/RecyclerView.kt
+++ app/src/main/java/com/example/bakerysraul/RecyclerView.kt
@@ -1,12 +1,31 @@
 package com.example.bakerysraul
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerView : AppCompatActivity() {
+    private lateinit var viewAdapter: ClientesAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    val cList: List<Clientes> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = ClientesAdapter(cList,this,{estud:Clientes->onItemclickListener(estud)})
+        //viewAdapter = ClientesAdapter(clientesList,this,{estud:Clientes -> onItemClickListener(estud)})
+
+        rv_clientes_list.apply{
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+            addItemDecoration(DividerItemDecoration(this@RecyclerView,
+                DividerItemDecoration.VERTICAL))
+        }
     }
 }
